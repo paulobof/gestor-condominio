c12b7178341979721c2751734b69c1d6b18fe04a
paulobof <paulobof@gmail.com>
2026-05-25 22:38:30 -0300

    feat(security): RateLimitFilter Bucket4j (login 5/min/IP refresh 10/min/IP)

---
+++ backend/src/main/java/br/com/condominio/shared/security/RateLimitProperties.java
@@ -0,0 +1,16 @@
+package br.com.condominio.shared.security;
+
+import java.time.Duration;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "app.ratelimit")
+public class RateLimitProperties {
+  private int loginPerMinPerIp = 5;
+  private int refreshPerMinPerIp = 10;
+  private int loginLockoutAttempts = 10;
+  private Duration loginLockoutWindow = Duration.ofMinutes(30);
+}
